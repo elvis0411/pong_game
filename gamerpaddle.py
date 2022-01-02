c3ebb9f1924bb92ebdb43b3dc699a140ab09e686
Elvis <91163415+elvis0411@users.noreply.github.com>
2022-01-02 01:11:25 -0800

    add player's paddle into the screen

---
+++ gamerpaddle.py
@@ -0,0 +1,10 @@
+# this is the part that control the player's paddle
+
+from turtle import Turtle
+
+class Gamerpaddle:
+    
+    def __init__(self):
+        
+        self.paddle = Turtle()
+        self.
